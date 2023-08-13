09633bec69d653d3f8d7db9a83f1c957c6a2df3b
Temzu <40913z@mail.ru>
2023-08-13 13:53:31 +0300

    Initial commit

---
+++ src/test/java/com/temzu/cafefreshmicroservices/CafeFreshMicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.temzu.cafefreshmicroservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafeFreshMicroservicesApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
